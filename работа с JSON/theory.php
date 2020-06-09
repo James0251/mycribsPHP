6857b64ff67271ab2f6a7ed29f5aed10d4a88d12
James0251 <den4643@yandex.ru>
2020-06-09 03:02:07 +0300

    тренируюсь с json

--- работа с JSON/theory.php
+++ работа с JSON/theory.php
@@ -1,36 +1,45 @@
 <?php
 $array = array(
-  '1' => 'значение 1',
-    '2' => 'значение 2',
-    '3' => 'значение 3',
-    '4' => 'значение 4',
-    '5' => 'значение 5'
+  [55.831903, 37.411961],
+  [55.763338, 37.565466],
+  [55.763338, 37.565466],
+  [55.744522, 37.616378],
+  [55.780898, 37.642889]
 );
-$json = json_encode($array, JSON_UNESCAPED_UNICODE);
+print_r($array);
+echo "<br/><br/>";
+
+echo gettype($array['1'])."<br/><br/>";
+
+$json = json_encode($array['1'], JSON_UNESCAPED_UNICODE);
 echo $json; echo "<br/><br/>";
 
-$arr = json_decode($json, true);
-switch (json_last_error()){
-    case JSON_ERROR_NONE:
-        echo "Ошибок нет <br/>";
-    break;
-    case JSON_ERROR_DEPTH:
-        echo "Достигнута максимальная глубина стека <br/>";
-    break;
-    case JSON_ERROR_STATE_MISMATCH:
-        echo "Некорректные разряды или несоответствие режимов <br/>";
-    break;
-    case JSON_ERROR_CTRL_CHAR:
-        echo "Некорректный управляющий символ <br/>";
-    break;
-    case JSON_ERROR_SYNTAX:
-        echo "Синтаксическая ошибка, некорректный JSON <br/>";
-    break;
-    case JSON_ERROR_UTF8:
-        echo "Некорректные символы UTF-8, возможно неверно закодирован <br/>";
-    break;
-    default:
-        echo "Неизвестная ошибка <br/>";
-    break;
-}
-print_r($arr);
+
+
+
+//
+//$arr = json_decode($json, true);
+//switch (json_last_error()){
+//    case JSON_ERROR_NONE:
+//        echo "Ошибок нет <br/>";
+//    break;
+//    case JSON_ERROR_DEPTH:
+//        echo "Достигнута максимальная глубина стека <br/>";
+//    break;
+//    case JSON_ERROR_STATE_MISMATCH:
+//        echo "Некорректные разряды или несоответствие режимов <br/>";
+//    break;
+//    case JSON_ERROR_CTRL_CHAR:
+//        echo "Некорректный управляющий символ <br/>";
+//    break;
+//    case JSON_ERROR_SYNTAX:
+//        echo "Синтаксическая ошибка, некорректный JSON <br/>";
+//    break;
+//    case JSON_ERROR_UTF8:
+//        echo "Некорректные символы UTF-8, возможно неверно закодирован <br/>";
+//    break;
+//    default:
+//        echo "Неизвестная ошибка <br/>";
+//    break;
+//}
+//print_r($arr);
